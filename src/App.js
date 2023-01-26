2ebf0612960e55125170f52bcedebfd2a3cf081c
BIP <satyam0150@gmail.com>
2023-01-26 11:07:30 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -12,23 +12,24 @@ import ProtectedRoute from './components/protected/ProtectedRoute';
 
 function App() {
   return (
-    <UserAuthContextProvider>
-      <Router>
+
+    <Router>
+      <UserAuthContextProvider>
         <Routes>
           <Route exact path="/" element={<Login />} ></Route>
-          <Route exact path="/home" 
-          element={
-          <ProtectedRoute>
-            <Home />
-            </ProtectedRoute>} >
-            </Route>
+          <Route exact path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>} >
+          </Route>
           <Route exact path="/signup" element={<Signup />} ></Route>
           <Route exact path="/navbar" element={<Navbar />} ></Route>
           <Route exact path="*" element={<Nopage />} ></Route>
         </Routes>
+      </UserAuthContextProvider>
+    </Router>
 
-      </Router>
-    </UserAuthContextProvider>
   );
 }
 
